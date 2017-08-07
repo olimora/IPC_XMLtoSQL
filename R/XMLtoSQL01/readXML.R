7f5a61f0ae5b5fb1b8c22d060089c93a08940b28
Moravcik <oliver.moravcik@adastragrp.com>
2017-08-07 14:58:08 +0200

    added selected columns and where conditions

--- R/XMLtoSQL01/readXML.R
+++ R/XMLtoSQL01/readXML.R
@@ -1,30 +1,56 @@
 library("xml2")
 
-input <- read_xml("example.xml")
+#############
+first_column_fl <- 1
+
+#############
+
+xml_input <- read_xml("example.xml")
 
 #find all sources
-source <- xml_find_all(input, ".//SOURCE")
+sources <- xml_find_all(xml_input, ".//SOURCE")
 
 part_select <- "SELECT"
 part_from <- "FROM"
-for (si in 1:length(source)) {
-  s_name <- xml_attr(source[si], "NAME")
-  print(s_name)
-  
-  s_children <- xml_children(source[si])
-  for (ci in 1:length(s_children)) {
-    c_name <- xml_attr(s_children[ci], "NAME")
-    #check for ',' at last position in SELECT part
+part_where <- "WHERE 1=1"
+for (si in 1:length(sources)) {
+
+  source_columns <- xml_children(sources[si])
+  for (ci in 1:length(source_columns)) {
+    column_name <- xml_attr(source_columns[ci], "NAME")
+    if (first_column_fl == 1) { 
+      first_column_fl <- 0
+      part_select <- paste(part_select, column_name)
+    } else { 
+      part_select <- paste0(part_select, ', ', column_name)
+    }
   }
   
+  source_name <- xml_attr(sources[si], "NAME")
+  source_alias <- paste0("s", si)
   if (si == 1) {  #from
-    part_from <- paste(part_from, s_name, paste0("s", si))
+    part_from <- paste(part_from, source_name, source_alias)
   } else {        #join
-    part_from <- paste(part_from, s_name, paste0("s", si))
+    part_from <- paste(part_from, source_name, source_alias)
   }
+  
+  sq_xpath <- paste0(".//TRANSFORMATION[@NAME='SQ_", source_name, "']")
+  source_qualifier <- xml_find_all(xml_input, sq_xpath)
+  condition_node <- xml_find_all(source_qualifier, "//TABLEATTRIBUTE[@NAME='Source Filter']")
+  if (length(condition_node) == 1) {
+    condition <- xml_attr(condition_node, "VALUE")
+    condition <- gsub("\\$\\$", "&", condition)
+    part_where <- paste0(part_where, " and ", source_alias, ".", condition)
+  } else {
+    #error nenasiel SQ alebo ich nasiel viac
+    print("error nenasiel SQ alebo ich nasiel viac")
+  }
+  
+  
 }
 
+####prepojit podla konektorov - najdem si source, spracujem, kuknem konektory, ktore idu z neho, kuknem kam idu, a z nich len uniqe, .. to ked idu viacere stlpce do jednoho stvorceka
 
-
-
+print(part_select)
 print(part_from)
+print(part_where)
