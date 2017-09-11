bfb3484e8f9f30ac8bab2b00403ed464e9079721
Moravcik <oliver.moravcik@adastragrp.com>
2017-09-11 13:25:28 +0200

    ends of branches found

--- R/XMLtoSQL01/branch_up.R
+++ R/XMLtoSQL01/branch_up.R
@@ -16,8 +16,7 @@ connectors <- get_connectors_dfs(xml_input)
   #to which more than one connectors go
   beg2 <- subset(as.data.frame(table(connectors$unique$to_instance), stringsAsFactors = F), Freq > 1)$Var1
   #obj do ktoreho idu konektory z objektu, ktory ma konektory do viacerych
-  #to which connectors go from object with connectors to more than one object
-  # targety objektu, z ktoreho ide viacero konektorov
+  #to which connectors go from object with connectors to more than one object / targets of those connectors
   beg3 <- subset(connectors$unique, from_instance %in% 
                    subset(as.data.frame(table(connectors$unique$from_instance), stringsAsFactors = F), Freq > 1)$Var1)$to_instance
   beginings <- unique(c(beg1, beg2, beg3))
@@ -25,6 +24,16 @@ connectors <- get_connectors_dfs(xml_input)
 
 
 ## find ends of branches
+{
+  #from which no connectors go
+  end1 <- subset(connectors$unique, !(to_instance %in% connectors$unique$from_instance))$to_instance 
+  #from which more than one connectors go
+  end2 <- subset(as.data.frame(table(connectors$unique$from_instance), stringsAsFactors = F), Freq > 1)$Var1
+  #from which connectors go to object with connectors from more than one object / sources of those connectors
+  end3 <- subset(connectors$unique, to_instance %in% 
+                   subset(as.data.frame(table(connectors$unique$to_instance), stringsAsFactors = F), Freq > 1)$Var1)$from_instance
+  endings <- unique(c(end1, end2, end3))
+}
 
 ## create branches
 
