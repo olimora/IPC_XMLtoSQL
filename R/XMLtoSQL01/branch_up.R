c0a7760c08080caf6b59717634119ec000a2e342
Moravcik <oliver.moravcik@adastragrp.com>
2017-09-11 14:32:11 +0200

    branches found and created, code refactored to functions

--- R/XMLtoSQL01/branch_up.R
+++ R/XMLtoSQL01/branch_up.R
@@ -8,34 +8,21 @@ xml_input <- read_xml("example.xml")
 
 ## get connectors
 connectors <- get_connectors_dfs(xml_input)
+## beginnings, endings
+beginnings <- get_branches_beginnings(connectors)
+endings <- get_branches_endings(connectors)
 
-## find begining of branches
-{
-  #to which no connectors go
-  beg1 <- subset(connectors$unique, !(from_instance %in% connectors$unique$to_instance))$from_instance 
-  #to which more than one connectors go
-  beg2 <- subset(as.data.frame(table(connectors$unique$to_instance), stringsAsFactors = F), Freq > 1)$Var1
-  #obj do ktoreho idu konektory z objektu, ktory ma konektory do viacerych
-  #to which connectors go from object with connectors to more than one object / targets of those connectors
-  beg3 <- subset(connectors$unique, from_instance %in% 
-                   subset(as.data.frame(table(connectors$unique$from_instance), stringsAsFactors = F), Freq > 1)$Var1)$to_instance
-  beginings <- unique(c(beg1, beg2, beg3))
-}
-
-
-## find ends of branches
-{
-  #from which no connectors go
-  end1 <- subset(connectors$unique, !(to_instance %in% connectors$unique$from_instance))$to_instance 
-  #from which more than one connectors go
-  end2 <- subset(as.data.frame(table(connectors$unique$from_instance), stringsAsFactors = F), Freq > 1)$Var1
-  #from which connectors go to object with connectors from more than one object / sources of those connectors
-  end3 <- subset(connectors$unique, to_instance %in% 
-                   subset(as.data.frame(table(connectors$unique$to_instance), stringsAsFactors = F), Freq > 1)$Var1)$from_instance
-  endings <- unique(c(end1, end2, end3))
-}
-
+match_ends <- vector(mode = "character", length = length(beginnings))
 ## create branches
+for (i in 1:length(beginnings)) {
+  end <- get_end_of_branch(connectors, beginnings[i], endings)
+  match_ends[i] <- end
+}
+
+branches <- data.frame(beg = beginnings, end = match_ends, preddisp = rep("", length(beginnings)),
+                       stringsAsFactors = F) #predispositions - tam bude proste string s nazvami objektov oddelenymi bodkociarkou
+
+
 
 ## find predispositions for branches
 
--- R/XMLtoSQL01/func__branching_utils.R
+++ R/XMLtoSQL01/func__branching_utils.R
@@ -3,6 +3,7 @@
 # functions used while managing branching
 # params: 
 #   xml_input - original XML doc with mapping.
+#   connectors - list with data about connectors, $uniqie, $all, $objects
 #
 ##################
 
@@ -28,3 +29,39 @@ get_connectors_dfs <- function(xml_input) {
   return(connectors)
 }
 
+## returns begining of branches
+get_branches_beginnings <- function(connectors) {
+  #to which no connectors go
+  beg1 <- subset(connectors$unique, !(from_instance %in% connectors$unique$to_instance))$from_instance 
+  #to which more than one connectors go
+  beg2 <- subset(as.data.frame(table(connectors$unique$to_instance), stringsAsFactors = F), Freq > 1)$Var1
+  #obj do ktoreho idu konektory z objektu, ktory ma konektory do viacerych
+  #to which connectors go from object with connectors to more than one object / targets of those connectors
+  beg3 <- subset(connectors$unique, from_instance %in% 
+                   subset(as.data.frame(table(connectors$unique$from_instance), stringsAsFactors = F), Freq > 1)$Var1)$to_instance
+  beginnings <- unique(c(beg1, beg2, beg3))
+  return(beginnings)
+}
+
+## returns ends of branches
+get_branches_endings <- function(connectors) {
+  #from which no connectors go
+  end1 <- subset(connectors$unique, !(to_instance %in% connectors$unique$from_instance))$to_instance 
+  #from which more than one connectors go
+  end2 <- subset(as.data.frame(table(connectors$unique$from_instance), stringsAsFactors = F), Freq > 1)$Var1
+  #from which connectors go to object with connectors from more than one object / sources of those connectors
+  end3 <- subset(connectors$unique, to_instance %in% 
+                   subset(as.data.frame(table(connectors$unique$to_instance), stringsAsFactors = F), Freq > 1)$Var1)$from_instance
+  endings <- unique(c(end1, end2, end3))
+  return(endings)
+}
+
+## returns end of branch
+get_end_of_branch <- function(connectors, beg, endings) {
+  if (beg %in% endings) {
+    return(beg)
+  } else {
+    next_obj <- subset(connectors$unique, from_instance == beg)$to_instance
+    get_end_of_branch(connectors, next_obj, endings)
+  }
+}
